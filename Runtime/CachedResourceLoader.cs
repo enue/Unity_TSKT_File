2b447c284e6f52a956422dee34d0229f53efbb02
enue <nnp@car.ocn.ne.jp>
2019-10-27 17:45:45 +0900

    CachedResourceLoader, WeakCachedResourceLoaderはyieldInstructionにする

--- Runtime/CachedResourceLoader.cs
+++ Runtime/CachedResourceLoader.cs
@@ -6,38 +6,57 @@ using UniRx.Async;
 
 namespace TSKT
 {
-    public static class CachedResourceLoader<T>
+    public class CachedResourceLoader<T> : CustomYieldInstruction
         where T : Object
     {
         readonly static Dictionary<string, T> cache = new Dictionary<string, T>();
 
-        public static T Load(string path)
+        UniTask<T> task;
+        public T Asset => task.Result;
+
+        public override bool keepWaiting => !task.IsCompleted;
+
+        public T Load(string path)
         {
             if (cache.TryGetValue(path, out var result))
             {
-                return result;
+                task = new UniTask<T>(result);
+                return Asset;
             }
 
-            result = Resources.Load<T>(path);
-            Debug.Assert(result, "not found entity : " + path);
+            task = new UniTask<T>(Resources.Load<T>(path));
+            Debug.Assert(Asset, "asset not found : " + path);
 
-            cache.Add(path, result);
-            return result;
+            cache.Add(path, Asset);
+            return Asset;
         }
 
-        async static public UniTask<T> LoadAsync(string path)
+        public UniTask<T> LoadAsync(string path, System.Action<T> callback = null)
         {
             if (cache.TryGetValue(path, out var asset))
             {
-                return asset;
+                task = new UniTask<T>(asset);
+                callback?.Invoke(asset);
+                return new UniTask<T>(asset);
             }
+
+            task = LoadAsyncCoroutine(path);
+            task.GetAwaiter().OnCompleted(() =>
+            {
+                cache[path] = Asset;
+                callback?.Invoke(Asset);
+            });
+
+            return task;
+        }
+
+        async static UniTask<T> LoadAsyncCoroutine(string path)
+        {
             var loader = new ResourceLoader<T>(path);
             await loader;
-            var result = loader.Asset;
-            Debug.Assert(result, "entity not found : " + path);
-            cache[path] = result;
-
-            return result;
+            var asset = loader.Asset;
+            Debug.Assert(asset, "asset not found : " + path);
+            return asset;
         }
 
         static public void Expire()
--- Runtime/WeakCachedResourceLoader.cs
+++ Runtime/WeakCachedResourceLoader.cs
@@ -6,67 +6,80 @@ using UniRx.Async;
 
 namespace TSKT
 {
-    public static class WeakCachedResourceLoader<T>
+    public class WeakCachedResourceLoader<T> : CustomYieldInstruction
         where T : Object
     {
-        static readonly Dictionary<string, System.WeakReference<T>> cache = new Dictionary<string, System.WeakReference<T>>();
+        static readonly Dictionary<string, System.WeakReference> cache = new Dictionary<string, System.WeakReference>();
+
+        UniTask<T> task;
+        public T Asset => task.Result;
 
         public static void TrimCache()
         {
             foreach (var it in cache.ToArray())
             {
-                if (!it.Value.TryGetTarget(out var t))
+                if (!it.Value.IsAlive)
                 {
                     cache.Remove(it.Key);
                 }
             }
         }
 
-        public static T Load(string path)
+        public override bool keepWaiting => !task.IsCompleted;
+
+        public T Load(string path)
         {
-            if (cache.TryGetValue(path, out var weakRef))
+            if (cache.TryGetValue(path, out var result))
             {
-                if (weakRef.TryGetTarget(out var instance))
+                if (result.IsAlive)
                 {
-                    if (instance)
-                    {
-                        return instance;
-                    }
+                    var asset = result.Target as T;
+                    task = new UniTask<T>(asset);
+                    return asset;
                 }
             }
 
-            var result = Resources.Load<T>(path);
-            Debug.Assert(result, "not found entity : " + path);
+            task = new UniTask<T>(Resources.Load<T>(path));
+            Debug.Assert(Asset, "asset not found : " + path);
 
-            if (weakRef != null)
+            if (result != null)
             {
-                weakRef.SetTarget(result);
+                result.Target = Asset;
             }
             else
             {
-                cache.Add(path, new System.WeakReference<T>(result));
+                cache.Add(path, new System.WeakReference(Asset));
             }
-            return result;
+            return Asset;
         }
 
-        async public static UniTask<T> LoadAsync(string path)
+        public UniTask<T> LoadAsync(string path, System.Action<T> callback = null)
         {
-            if (cache.TryGetValue(path, out var weakRef))
+            if (cache.TryGetValue(path, out var asset))
             {
-                if (weakRef.TryGetTarget(out var t))
+                if (asset.IsAlive)
                 {
-                    if (t)
-                    {
-                        return t;
-                    }
+                    task = new UniTask<T>(asset.Target as T);
+                    callback?.Invoke(Asset);
+                    return new UniTask<T>(Asset);
                 }
             }
+
+            task = LoadAsyncCoroutine(path);
+            task.GetAwaiter().OnCompleted(() =>
+            {
+                cache[path] = new System.WeakReference(Asset);
+                callback?.Invoke(Asset);
+            });
+
+            return task;
+        }
+        static async UniTask<T> LoadAsyncCoroutine(string path)
+        {
             var loader = new ResourceLoader<T>(path);
             await loader;
-            var result = loader.Asset;
-            Debug.Assert(result, "entity not found : " + path);
-            cache[path] = new System.WeakReference<T>(result);
-            return result;
+            Debug.Assert(loader.Asset, "asset not found : " + path);
+            return loader.Asset;
         }
 
         static public void Expire()
