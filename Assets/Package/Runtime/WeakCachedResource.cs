97ac0299550cec096b5d85cecd1ba9aa46c3b620
enue <otoriden@gmail.com>
2022-04-12 09:12:20 +0900

    0.17.1 Obsolete WeakCachedResource

--- Assets/Package/Runtime/WeakCachedResource.cs
+++ Assets/Package/Runtime/WeakCachedResource.cs
@@ -7,76 +7,25 @@ using Cysharp.Threading.Tasks;
 
 namespace TSKT
 {
+    [System.Obsolete]
     public static class WeakCachedResource<T>
         where T : Object
     {
-        static readonly Dictionary<string, System.WeakReference<T>> cache = new Dictionary<string, System.WeakReference<T>>();
-
         public static void TrimCache()
         {
-            foreach (var it in cache.ToArray())
-            {
-                if (!it.Value.TryGetTarget(out var _))
-                {
-                    cache.Remove(it.Key);
-                }
-            }
         }
 
         public static T? Load(string path)
         {
-            if (cache.TryGetValue(path, out var weakRef))
-            {
-                if (weakRef.TryGetTarget(out var asset))
-                {
-                    if (asset)
-                    {
-                        return asset;
-                    }
-                }
-            }
-
-            var result = Resources.Load<T>(path);
-
-            if (result)
-            {
-                if (weakRef != null)
-                {
-                    weakRef.SetTarget(result);
-                }
-                else
-                {
-                    cache[path] = new System.WeakReference<T>(result);
-                }
-            }
-            return result;
+            return Resources.Load<T>(path);
         }
 
         public async static UniTask<T?> LoadAsync(string path)
         {
-            if (cache.TryGetValue(path, out var weakRef))
-            {
-                if (weakRef.TryGetTarget(out var asset))
-                {
-                    if (asset)
-                    {
-                        return asset;
-                    }
-                }
-            }
-
-            var result = await ResourcesUtil.LoadAsync<T>(path);
-            if (result)
-            {
-                cache[path] = new System.WeakReference<T>(result!);
-            }
-
-            return result;
+            return await ResourcesUtil.LoadAsync<T>(path);
         }
-
         public static void Expire()
         {
-            cache.Clear();
         }
     }
 }
