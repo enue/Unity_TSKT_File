22ddf385f6689c0b174251829a6dd280b5586562
enue <otoriden@gmail.com>
2022-11-06 15:07:36 +0900

    初期化ベクトルが毎回変化するよう修正

--- Assets/Package/Runtime/CryptUtil.cs
+++ Assets/Package/Runtime/CryptUtil.cs
@@ -11,6 +11,12 @@ namespace TSKT
 {
     public static class CryptUtil
     {
+        public static void GenerateKey(out byte[] key, string password, byte[] salt, int iterations, int keySize)
+        {
+            // パスワードから共有キーを作成
+            using var deriveBytes = new Rfc2898DeriveBytes(password, salt, iterations: iterations);
+            key = deriveBytes.GetBytes(keySize / 8);
+        }
         public static void GenerateKeyAndIV(out byte[] key, out byte[] iv, string password, byte[] salt, int iterations, int keySize, int blockSize)
         {
             // パスワードから共有キーと初期化ベクターを作成
@@ -24,9 +30,8 @@ namespace TSKT
 
             result.KeySize = 128;
             result.BlockSize = 128;
-            GenerateKeyAndIV(out var key, out var iv, password, salt, iterations, result.KeySize, result.BlockSize);
+            GenerateKey(out var key, password, salt, iterations, result.KeySize);
             result.Key = key;
-            result.IV = iv;
             return result;
         }
 
@@ -36,7 +41,12 @@ namespace TSKT
             {
                 using (var encryptor = aes.CreateEncryptor())
                 {
-                    return encryptor.TransformFinalBlock(bytes, 0, bytes.Length);
+                    var iv = aes.IV;
+                    var body = encryptor.TransformFinalBlock(bytes, 0, bytes.Length);
+                    var result = new byte[iv.Length + body.Length];
+                    Array.Copy(iv, result, iv.Length);
+                    Array.Copy(body, 0, result, iv.Length, body.Length);
+                    return result;
                 }
             }
         }
@@ -45,9 +55,23 @@ namespace TSKT
         {
             using (var aes = CreateAes(key, salt, iterations))
             {
-                using (var decryptor = aes.CreateDecryptor())
+                try
                 {
-                    return decryptor.TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);
+                    var iv = encryptedBytes.AsSpan(0, aes.BlockSize / 8).ToArray();
+                    aes.IV = iv;
+                    using (var decryptor = aes.CreateDecryptor())
+                    {
+                        return decryptor.TransformFinalBlock(encryptedBytes, iv.Length, encryptedBytes.Length - iv.Length);
+                    }
+                }
+                catch (CryptographicException)
+                {
+                    GenerateKeyAndIV(out _, out var iv, key, salt, iterations, aes.KeySize, aes.BlockSize);
+                    aes.IV = iv;
+                    using (var decryptor = aes.CreateDecryptor())
+                    {
+                        return decryptor.TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);
+                    }
                 }
             }
         }
