94052946d661a499cf1c85441be75aa12a587f83
enu <otoriden@gmail.com>
2026-07-28 08:19:26 +0900

    Update LoadingIndicator.cs

--- Assets/Package/Runtime/LoadingIndicator.cs
+++ Assets/Package/Runtime/LoadingIndicator.cs
@@ -29,7 +29,7 @@ namespace TSKT
             try
             {
                 var cts = CancellationTokenSource.CreateLinkedTokenSource(ct);
-                progress.OperationCount.Where(_ => _ == 0).Take(1).Subscribe(_ => cts.Cancel()).RegisterTo(ct);
+                progress.OperationCount.Where(_ => _ == 0).TakeUntil(ct).Take(1).DoCancelOnCompleted(cts);
 
                 await Observable.Timer(System.TimeSpan.FromSeconds(delay)).FirstAsync(cts.Token);
 
