576e08c84634ac35c51e37d206807e3466d94a1c
enu <otoriden@gmail.com>
2026-07-28 09:25:44 +0900

    obsolete CachedResource

--- Assets/Package/Runtime/CachedResource.cs
+++ Assets/Package/Runtime/CachedResource.cs
@@ -6,46 +6,23 @@ using System.Linq;
 
 namespace TSKT
 {
+    [System.Obsolete]
     public static class CachedResource<T>
         where T : Object
     {
-        readonly static Dictionary<string, T?> cache = new();
 
         public static T? Load(string path)
         {
-            if (cache.TryGetValue(path, out var result))
-            {
-                if (result)
-                {
-                    return result;
-                }
-            }
-
-            result = Resources.Load<T>(path);
-
-            cache[path] = result;
-            return result;
+            return Resources.Load<T>(path);
         }
 
         public async static Awaitable<T?> LoadAsync(string path)
         {
-            if (cache.TryGetValue(path, out var result))
-            {
-                if (result)
-                {
-                    return result;
-                }
-            }
-
-            result = await ResourcesUtil.LoadAsync<T>(path, _ => LoadingProgress.Instance.Observe(_));
-            cache[path] = result;
-
-            return result;
+            return await ResourcesUtil.LoadAsync<T>(path, _ => LoadingProgress.Instance.Observe(_));
         }
 
         public static void Expire()
         {
-            cache.Clear();
         }
     }
 }
